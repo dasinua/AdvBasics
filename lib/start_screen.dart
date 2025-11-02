5223d029d7c387c32f4f242cf19b77bde2d55043
dasinua <dasinua@gmail.com>
2025-11-02 16:55:58 +0200

    opacity

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,6 +5,38 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return Center(child: Text('Aleksandr'));
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Opacity(
+            opacity: 0.6,
+            child: Image.asset(
+              'assets/images/quiz-logo.png',
+              width: 300,
+            ),
+          ),
+          SizedBox(height: 80),
+          Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Color.fromARGB(255, 237, 223, 252),
+              fontSize: 24,
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: Icon(Icons.arrow_right_alt),
+            label: Text('Start Quiz'),
+          ),
+        ],
+      ),
+    );
   }
 }
